4b1f924690c47fab15ea4168bac666f952af2d54
Your Name <you@example.com>
2024-11-11 22:42:51 +0700

    env

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from api.route import route_blueprint
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('../config.py')
     app.secret_key = 'secret'
     app.register_blueprint(route_blueprint)
     
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = {
+    'apiKey': os.environ.get('API_KEY'),
+    'authDomain': os.environ.get('AUTH_DOMAIN'),
+    'projectId': os.environ.get('PROJECT_ID'),
+    'storageBucket': os.environ.get('STORAGE_BUCKET'),
+    'messagingSenderId': os.environ.get('MSG_ID'),
+    'appId': os.environ.get('APP_ID'),
+    'measurementId': os.environ.get('MEASURE_ID'),
+    'signInFlow': os.environ.get('FLOW'),
+    'databaseURL': os.environ.get('DB_URL')
+}
+print(config)
